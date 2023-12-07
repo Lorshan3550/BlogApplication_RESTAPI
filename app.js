91c8838e5453f9571a4dd6e8245f796539f18104
Lorshan3550 <lorshannarayanasamy@gmail.com>
2023-12-07 11:09:37 +0530

    Create signup route and its middleware

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes"
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
-dotenv.config()
+const app = express();
+const port = 5000;
+const username = process.env.USER;
+const password = process.env.PASSWORD;
+const dbName = "Blog";
 
+app.use(express.json()) // It indicates what type of data receieved from the frontend to server
 
-const app = express()
-const port = 5000
-const username = process.env.USER
-const password = process.env.PASSWORD
-const dbName = "Blog"
-
-// console.log(username)
-// console.log(password)
-
-
-// app.use('/api', (req , res , next) => {
-//     res.send("Hello World")
-// })
-
-
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.okpeanj.mongodb.net/${dbName}?retryWrites=true&w=majority`
-).then(() => app.listen(5000)).
-then(() => console.log("Connected TO Database and Listening TO Localhost 5000")).
-catch((err) => console.log(err))
+app.use("/api/user", router) // http://localhost:5000/api/user/logon
 
 
 
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@cluster0.okpeanj.mongodb.net/${dbName}?retryWrites=true&w=majority`
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
---
+++ controllers/user-controller.js
@@ -0,0 +1,48 @@
+import User from "../model/User.js"
+import bcrypt from "bcryptjs"
+
+
+export const getAllUser = async  (req, res, next) => {
+    let users;
+    try{
+        users = await User.find();
+    } catch(err) {
+        console.log(err)
+    }
+
+    if(!users) {
+        return res.status(404).json({message: "No users Found"})
+    }
+    return res.status(200).json({users})
+}
+
+export const signup = async (req,res,next) => {
+    const {name, email, password} = req.body
+    let existingUser
+
+    try{
+        existingUser = await User.findOne({email})
+    } catch(err){
+        console.log(err)
+    }
+
+    if(existingUser){
+        return res.status(400).json({message: "User Already Exists! Login Instead"})
+    }
+
+    const hashedPassword = bcrypt.hashSync(password)
+
+    const user = new User({
+        name,  // name : name
+        email,
+        password: hashedPassword
+    })
+
+    try{
+        await user.save()
+    } catch(err){
+        console.log(err)
+    }
+
+    return res.status(201).json({user})
+}
