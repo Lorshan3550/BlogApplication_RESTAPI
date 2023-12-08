52eff4360b3953e2839ff4a717a0e44ba758d753
Lorshan3550 <lorshannarayanasamy@gmail.com>
2023-12-08 07:58:51 +0530

    Create login route and its middleware

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -46,3 +46,25 @@ export const signup = async (req,res,next) => {
 
     return res.status(201).json({user})
 }
+
+
+export const login = async (req,res,next) => {
+    const {email, password} = req.body
+    let existingUser
+
+    try{
+        existingUser = await User.findOne({email})
+    } catch(err){
+        console.log(err)
+    }
+
+    if(!existingUser){
+        return res.status(404).json({message: "Couldnt Find User By This Email"})
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password)
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Incorrect Password"})
+    }
+    return res.status(200).json({message:"Login Successfull"})
+}
